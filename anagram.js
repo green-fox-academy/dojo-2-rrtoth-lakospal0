70de6c4d45492906abc9218fa7b0d50f5c91053b
rekatoth <reka.rebeka.toth@gmail.com>
2019-11-11 10:25:25 +0100

    exercise 2 - test 2

--- anagram.js
+++ anagram.js
@@ -5,13 +5,13 @@ let test = require('tape');
 const anagram = (a, b) => {
     let first = a.split('').sort().join('');
     let second = b.split('').sort().join('');
+    
     if (first == second) {
         return true;
     } else {
         return false;
     }
 }
-
 
 //test #1
 
@@ -26,12 +26,12 @@ test("anagram", t => {
     t.end()
 })
 
-//test2
-let a = "qwerty"
-let b = "ytrewq"
+//test #2
+let c = "qwerty"
+let d = "ytr e wq"
 
 test("anagram", t => {
-    let actual = anagram(a, b);
+    let actual = anagram(c, d);
     let expected = true;
 
     t.equal(actual, expected)
